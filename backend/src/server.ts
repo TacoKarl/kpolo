e7a6aee8225531b1ccf22bcc80433f01d8bf1995
TacoKarl <oliver.rosenkilde@gmail.com>
2026-02-12 11:15:32 +0100

    compose spinder det hele op. Husk at lave en .env til dig selv inden du spinder det op for at teste (#36)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,39 @@
 import express from "express";
+import dotenv from "dotenv";
+import {Pool} from "pg";
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.use(express.json());
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
+pool.connect()
+    .then(() => {
+      console.log("Forbundet til PostgreSQL");
+    })
+    .catch((err) => {
+      console.error("Kunne ikke forbinde til DB ", err);
+    });
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
+});
+
+app.get("/db-test", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({
+      message: "Database virker!",
+      time: result.rows[0],
+    });
+  } catch (error) {
+    res.status(500).json({ error: "DB fejl", details: error });
+  }
 });
 
 app.listen(port, () => {
