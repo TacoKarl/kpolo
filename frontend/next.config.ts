d70b4802fd08bd862af8facdf40333ff829b32f9
Oliver Bjørn Diangson Rosenkilde <olros.dev@proton.me>
2026-02-21 14:04:00 +0100

    security policy til cloudflare

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,29 +1,8 @@
 import type { NextConfig } from "next";
 
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' https://challenges.cloudflare.com;
-  frame-src https://challenges.cloudflare.com;
-  connect-src 'self' https://challenges.cloudflare.com;
-  style-src 'self' 'unsafe-inline';
-  img-src 'self' data:;
-`;
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: ContentSecurityPolicy.replace(/\n/g, ""),
-          },
-        ],
-      },
-    ];
-  },
 };
 export default nextConfig;
