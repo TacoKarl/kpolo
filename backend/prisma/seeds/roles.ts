d498f38b01bf9f4ff1948aa2b3c42ab0621758b5
ThorMcQ <202205431@post.au.dk>
2026-03-05 13:57:09 +0100

    Added seed functionality to prisma, for roles

---
+++ backend/prisma/seeds/roles.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '../../src/generated/prisma';
+
+
+export async function seedRoles(prisma: PrismaClient) {
+  const roles = ['System Admin', 'Club Admin', 'Guest'];
+  for (const role of roles) {
+    await prisma.role.upsert({
+      where: { role },
+      update: {},
+      create: { role },
+    });
+  }
+};
