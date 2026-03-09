ef797529a299bf062a8ba724f7bc6fb5e72c1db6
ThorMcQ <202205431@post.au.dk>
2026-03-09 13:47:59 +0100

    Added fakeData seed

--- backend/prisma/seeds/roles.ts
+++ backend/prisma/seeds/roles.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '../../src/generated/prisma';
 
 
 export async function seedRoles(prisma: PrismaClient) {
-  const roles = ['System Admin', 'Club Admin', 'Guest'];
+  const roles = ['System Admin', 'Club Admin','Club Member', 'Guest'];
   for (const role of roles) {
     await prisma.role.upsert({
       where: { role },
